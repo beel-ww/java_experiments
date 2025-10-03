b239bd31cf1c8c4a721a58c70982f6d2014765cc
Bill <bliao6@proton.me>
2025-10-03 13:10:29 -0700

    trying exceptions

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         try {reader("src/DinoDiet.txt");}
         catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
+
+        badArgs(-1, "Joe");
+        badArgs(15, "Joe");
     }
 
     public static void reader(String filename) throws IOException {
@@ -27,5 +30,32 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Error opening file " + filename);
         }
+    }
+
+    public static void badArgs(int age, String name) throws IllegalArgumentException {
+        try {
+            Dinosaur d = new Dinosaur(age, name);
+            System.out.println(d.getInfo());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class Dinosaur {
+    final private int age;
+    final private String name;
+
+    Dinosaur(int age, String name) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+
+        this.age = age;
+        this.name = name;
+    }
+
+    String getInfo() {
+        return this.name + " is " + this.age + " years old.";
     }
 }
